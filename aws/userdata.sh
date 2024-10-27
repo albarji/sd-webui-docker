e0fb6ff28e0903ba98ba6dbeb768e5040f4d00d5
Álvaro Barbero Jiménez <albarjip@gmail.com>
2024-10-27 12:04:41 +0100

    Fixed docker folder configuration

--- aws/userdata.sh
+++ aws/userdata.sh
@@ -21,8 +21,10 @@ yum-config-manager --disable amzn2-graphics  # Disable AWS repo because it conta
 yum install -y nvidia-container-toolkit
 # Install docker: use the external NVMe disk to store all docker information
 amazon-linux-extras install -y docker
+# Configure docker to use the external NVMe disk to store all docker information
 mkdir /data/docker
-sed -i 's/"$/ -g \/data\/docker"/' /etc/sysconfig/docker
+cat '{"data-root": "/data/docker"}' > /etc/docker/daemon.json
+# Start docker service
 service docker start
 # Run SD web UI, serving on port 80
 docker run --gpus all --rm -p 80:7860 albarji/sd-webui:1.1.0
